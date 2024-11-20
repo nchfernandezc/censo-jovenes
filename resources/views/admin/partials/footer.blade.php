a6568a68b2cb6b3e47d63fc4332631f2333498d4
Eduardo <eduzpa8@gmail.com>
2024-11-20 11:05:00 -0400

    ingreso del link en el footer

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
-          <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2024. <a href="https://gobernaciondelzulia.gob.ve" ></a> Gobernación del Estado Zulia. Todos los Derechos Reservados.</span>
-          </div>
-        </footer>
+  <div class="d-sm-flex justify-content-center justify-content-sm-between">
+    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2024. <a href="https://gobernaciondelzulia.gob.ve">Gobernación del Estado Zulia. </a> Todos los Derechos Reservados.</span>
+  </div>
+</footer>
